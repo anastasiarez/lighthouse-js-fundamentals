02a3ff50074bbf902e3a1420bcfedee4421bbe5c
Mona Lisa <anastasiia.rn@gmail.com>
2023-04-06 21:28:33 -0400

    more examples

--- conditionals.js
+++ conditionals.js
@@ -7,3 +7,19 @@ console.log("It's nice, but not hot");
 } else if (temp > 15){
   console.log("It's hot!");
 }
+
+const isCitizen = true;
+const age = 33;
+
+if(isCitizen && age > 18) {
+  console.log("You are eligible to vote.");
+}
+
+var temperature
+if (temperature < -40 || temperature >40) {
+  console.log("Do not go outside today");
+}
+
+if(!raining){
+  console.log("Leave your umbrella at home");
+}
