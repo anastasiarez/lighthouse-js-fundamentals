b044fe8603b755e3e29df1700d8e53884ed57c2a
Mona Lisa <anastasiia.rn@gmail.com>
2023-04-06 21:22:27 -0400

    Weather exercise

---
+++ conditionals.js
@@ -0,0 +1,9 @@
+const temp = 16;
+
+if (temp < 0) {
+  console.log("Its cold!");
+} else if (temp <= 15) {
+console.log("It's nice, but not hot");
+} else if (temp > 15){
+  console.log("It's hot!");
+}
