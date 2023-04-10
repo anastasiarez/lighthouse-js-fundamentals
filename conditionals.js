9163f793d43b24c8fcffd896368b51cc8cd7a343
Mona Lisa <anastasiia.rn@gmail.com>
2023-04-10 10:12:24 -0400

    practicing

--- conditionals.js
+++ conditionals.js
@@ -3,23 +3,23 @@ const temp = 16;
 if (temp < 0) {
   console.log("Its cold!");
 } else if (temp <= 15) {
-console.log("It's nice, but not hot");
-} else if (temp > 15){
+  console.log("It's nice, but not hot");
+} else if (temp > 15) {
   console.log("It's hot!");
 }
 
 const isCitizen = true;
 const age = 33;
 
-if(isCitizen && age > 18) {
+if (isCitizen && age > 18) {
   console.log("You are eligible to vote.");
 }
 
 var temperature
-if (temperature < -40 || temperature >40) {
+if (temperature < -40 || temperature > 40) {
   console.log("Do not go outside today");
 }
 
-if(!raining){
+if (!raining) {
   console.log("Leave your umbrella at home");
 }
